c705c3068f80c77f6d3c349ee72b0c631cff19a1
Rishi Bagora <codzenrb@gmail.com>
2026-02-21 09:40:18 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import ScrollToTop from './components/common/ScrollToTop.jsx';
-
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
 
@@ -12,6 +12,7 @@ createRoot(document.getElementById('root')).render(
 
     <ScrollToTop/>
   <App />
+  <Analytics/>
 
 
     
